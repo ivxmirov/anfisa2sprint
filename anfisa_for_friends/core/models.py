4d57f6907e875b7dcb8277bb33f14fe0f65ca206
Илья Хмыров <ivxmirov@gmail.com>
2024-05-09 18:24:02 +0400

    after localization and translation

--- anfisa_for_friends/core/models.py
+++ anfisa_for_friends/core/models.py
@@ -5,3 +5,7 @@ class PublishedModel(models.Model):
     """Абстрактная модель. Добвляет флаг is_published."""
     is_published = models.BooleanField('Опубликовано', default=True)
     title = models.CharField('Название', max_length=256)
+
+
+    class Meta:
+        abstract = True
