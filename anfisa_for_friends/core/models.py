b85395b341270281ddc1fb47fdca77c679d436a0
Илья Хмыров <ivxmirov@gmail.com>
2024-05-09 18:22:57 +0400

    after localization and translation

--- anfisa_for_friends/core/models.py
+++ anfisa_for_friends/core/models.py
@@ -3,7 +3,5 @@ from django.db import models
 
 class PublishedModel(models.Model):
     """Абстрактная модель. Добвляет флаг is_published."""
-    is_published = models.BooleanField(default=True)
-
-    class Meta:
-        abstract = True
+    is_published = models.BooleanField('Опубликовано', default=True)
+    title = models.CharField('Название', max_length=256)
